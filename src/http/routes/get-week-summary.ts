3e928de44b2b9ccdbd992ecff609a747416b751f
joschonarth <joschonarth@gmail.com>
2026-02-27 18:53:21 -0300

    feat: implement weekly summary navigation

--- src/http/routes/get-week-summary.ts
+++ src/http/routes/get-week-summary.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod'
 import { getWeekSummary } from '../../functions/get-week-summary'
@@ -11,6 +12,12 @@ export const getWeekSummaryRoute: FastifyPluginAsyncZod = async app => {
       schema: {
         tags: ['goals'],
         description: 'Get week summary',
+        querystring: z.object({
+          weekStartsAt: z.coerce
+            .date()
+            .optional()
+            .default(dayjs().startOf('week').toDate()),
+        }),
         response: {
           200: z.object({
             summary: z.object({
@@ -33,8 +40,11 @@ export const getWeekSummaryRoute: FastifyPluginAsyncZod = async app => {
     },
     async request => {
       const userId = request.user.sub
+      const { weekStartsAt } = request.query
+
       const { summary } = await getWeekSummary({
         userId,
+        weekStartsAt,
       })
 
       return { summary }
